12cc4eaf0fa2d97e89ac34973fd4f36e2bb2355d
Adam Dřínek <adam.drinek@gmail.com>
2023-03-31 00:01:33 +0200

    change colors

--- imports/ui/components/RenderingEngine.ts
+++ imports/ui/components/RenderingEngine.ts
@@ -36,12 +36,25 @@ class RenderingEngine
             }
         }
 
+        /*const oldC = hexToRgb('#000000')!
+        const newC = hexToRgb('#1919A6')!
+        this.recolorImage(oldC.r,oldC.g,oldC.b,newC.r,newC.g,newC.b)
+
+        const old2 = hexToRgb('#ffffff')!
+        const new2 = hexToRgb('#A1045A'/*'#BF00FF'/*'#301934'/*'#593163'*//*)!
+        this.recolorImage(old2.r,old2.g,old2.b,new2.r,new2.g,new2.b)*/
+
         // render entities
         this.data.forEach(ent => {
             const offsetX = ent.spriteMapOffset[0] + RenderingEngine.facingDirToOffset(ent)
             const offsetY = ent.spriteMapOffset[1]
             this.drawSprite(offsetX, offsetY, ent.position[0], ent.position[1])
         })
+
+        this.drawSprite(4, 2, 9, 11)
+        this.drawSprite(5, 2, 5, 6)
+        this.drawSprite(6, 2, 13, 4)
+        this.drawSprite(7, 2, 15, 10)
     }
 
     loadImage = () => {
@@ -81,6 +94,45 @@ class RenderingEngine
             default: return 0
         }
     }
+
+    // @ts-ignore
+    /*recolorImage = (oldRed,oldGreen,oldBlue,newRed,newGreen,newBlue) => {
+
+    const w = this.canvas.width
+    const h = this.canvas.height
+
+    // pull the entire image into an array of pixel data
+    var imageData = this.ctx.getImageData(0, 0, w, h);
+
+    // examine every pixel, 
+    // change any old rgb to the new-rgb
+    for (var i=0;i<imageData.data.length;i+=4)
+      {
+          // is this pixel the old rgb?
+          if(imageData.data[i]==oldRed &&
+             imageData.data[i+1]==oldGreen &&
+             imageData.data[i+2]==oldBlue
+          ){
+              // change to your new rgb
+              imageData.data[i]=newRed;
+              imageData.data[i+1]=newGreen;
+              imageData.data[i+2]=newBlue;
+          }
+      }
+    // put the altered data back on the canvas  
+    this.ctx.putImageData(imageData,0,0);
+    // put the re-colored image back on the image
+
+    }*/
 }
+
+/*function hexToRgb(hex: string) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }*/
 
 export default RenderingEngine
